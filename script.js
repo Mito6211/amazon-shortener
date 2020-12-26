66d225282e7cd4990f7e6f5b2dd5b6d5cf2077e1
Mito6211 <willmoore674@gmail.com>
2020-12-26 12:27:31 -0500

    Added success and error messages for clipboard

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const urlInput = document.getElementById("url-input");
 const shortenBtn = document.getElementById("shorten-btn");
 const output = document.getElementById("output");
+const errorMessage = document.getElementById("error-message");
+
 let link;
 
 shortenBtn.addEventListener("click", () => {
-    const inputText = urlInput.value.replace("product/", "");
-    const beginningURL = inputText.match(/amazon\.(\w|\.)+/gim)[0];
-    const importantInfo = inputText.match(/\/dp\/.{10}/gim)[0];
-    output.textContent = beginningURL + importantInfo;
-    link = "https://www." + beginningURL + importantInfo;
-    // make a href
+    try {
+        const inputText = urlInput.value.replace("product/", "");
+        const beginningURL = inputText.match(/amazon\.(\w|\.)+/gim)[0];
+        const importantInfo = inputText.match(/\/dp\/.{10}/gim)[0];
+        output.textContent = beginningURL + importantInfo;
+        link = "https://www." + beginningURL + importantInfo;
+    } catch {
+        console.log("invalid link");
+    }
 });
 
 output.addEventListener("click", () => {
     navigator.clipboard.writeText(link).then(
         () => {
-            console.log("copied!");
+            errorMessage.textContent = "Copied!";
+            errorMessage.style.backgroundColor = "#33996999";
+            errorMessage.classList.remove("hidden");
         },
         () => {
-            console.log("failed to copy.");
+            errorMessage.textContent = "Failed to copy.";
+            errorMessage.classList.remove("hidden");
         }
     );
 });
